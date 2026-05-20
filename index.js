454d2a2e69d23160b6000687ea9d66a89f7d5941
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-20 23:36:17 +0200

    implementato la chat tramite console con CLAUDIO STEGOSAURO

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ import { cowsay } from "cowsayjs";
 import { ChatAnthropic } from "@langchain/anthropic";
 import { HumanMessage } from "langchain";
 
-const CLAUDE_API_KEY= process.env.CLAUDE_API_KEY;
+const CLAUDE_API_KEY = process.env.CLAUDE_API_KEY;
+let valoriPassatiDaCmd = process.argv.slice(2);
 
 const model = new ChatAnthropic({
     model: 'claude-haiku-4-5-20251001',
     apiKey: CLAUDE_API_KEY,
 });
 
-model.invoke([
-    new HumanMessage('mi dici una barzelletta?')
-]).then(aiResponse => {
-    console.log(aiResponse.content);
-});
 
-const valoriPassatiDaCmd = process.argv;
-const outputDrago = cowsay("sono il fantasma formaggino", {cow: 'stegosaurus'})
+if (valoriPassatiDaCmd.length === 0) {
+    console.log('PRIMA DI TUTTO SCRIVI UN COMANDO DA MANDARE A CLAUDIO-AI SCRIVENDO IL COMANDO "pnpm start "stringa come prompt" RICEVERAI RISPOSTA DA UN CLAUDIO-AI-STEGOSAURO" ');
+} else {
+    const stringaDiComandoRicevuta = valoriPassatiDaCmd.join(" ");
+    model.invoke([
+        new HumanMessage(stringaDiComandoRicevuta)
+    ]).then(aiResponse => {
+        console.log(cowsay(aiResponse.content, { cow: 'stegosaurus' }));
+    });
+}
