228bc5b8f21b339e93c1297d75d3da1b26d0f9bd
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-20 23:04:58 +0200

    implementato la lettura del file env sia a start che watch nello script e implementata la IA di CLAUDIO

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import { cowsay } from "cowsayjs";
+import { ChatAnthropic } from "@langchain/anthropic";
+import { HumanMessage } from "langchain";
 
-console.log('hello world');
+const CLAUDE_API_KEY= process.env.CLAUDE_API_KEY;
+
+const model = new ChatAnthropic({
+    model: 'claude-haiku-4-5-20251001',
+    apiKey: CLAUDE_API_KEY,
+});
+
+model.invoke([
+    new HumanMessage('mi dici una barzelletta?')
+]).then(aiResponse => {
+    console.log(aiResponse.content);
+});
 
 const valoriPassatiDaCmd = process.argv;
 const outputDrago = cowsay("sono il fantasma formaggino", {cow: 'stegosaurus'})
-
-console.log(outputDrago)
